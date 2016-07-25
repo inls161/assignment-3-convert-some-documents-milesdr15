48ae39238ef3c744e5425b6f042887d99ac40748
milesdr <milesdr@live.unc.edu>
2016-07-25 11:29:46 -0400

    tested convert

--- milesdr15-convert-docs.sh
+++ milesdr15-convert-docs.sh
@@ -1,13 +1,14 @@
-
+#!/bin/bash
 
 # Declare input argument as a variable
-
+INPUTFILE=$1
 # Convert markdown to HTML
-
+pandoc -o $INPUTFILE.html $INPUTFILE
 # Convert markdown to DOCX
-
+pandoc -o $INPUTFILE.docx $INPUTFILE
 # Convert markdown to ODT
-
+pandoc -o $INPUTFILE.odt $INPUTFILE
 # Convert markdown to PDF
-
+pandoc -o $INPUTFILE.pdf $INPUTFILE
 # Make the command line read "Converted INPUT-FILENAME to HTML, DOCX, ODT, PDF
+echo "Converted $INPUTFILE to HTML, DOCX, ODT, PDF"
